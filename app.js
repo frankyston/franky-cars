44fdfc597ebeae17550445848320e8f6455dbfa6
Frankyston Lins <frankyston@gmail.com>
2017-12-24 18:22:43 -0300

    challenge-33

--- app.js
+++ app.js
@@ -1,39 +1,6 @@
 (function(win, doc) {
   'use strict';
-  /*
-  Vamos estruturar um pequeno app utilizando módulos.
-  Nosso APP vai ser um cadastro de carros. Vamos fazê-lo por partes.
-  A primeira etapa vai ser o cadastro de veículos, de deverá funcionar da
-  seguinte forma:
-  - No início do arquivo, deverá ter as informações da sua empresa - nome e
-  telefone (já vamos ver como isso vai ser feito)
-  - Ao abrir a tela, ainda não teremos carros cadastrados. Então deverá ter
-  um formulário para cadastro do carro, com os seguintes campos:
-    - Imagem do carro (deverá aceitar uma URL)
-    - Marca / Modelo
-    - Ano
-    - Placa
-    - Cor
-    - e um botão "Cadastrar"
 
-  Logo abaixo do formulário, deverá ter uma tabela que irá mostrar todos os
-  carros cadastrados. Ao clicar no botão de cadastrar, o novo carro deverá
-  aparecer no final da tabela.
-
-  Agora você precisa dar um nome para o seu app. Imagine que ele seja uma
-  empresa que vende carros. Esse nosso app será só um catálogo, por enquanto.
-  Dê um nome para a empresa e um telefone fictício, preechendo essas informações
-  no arquivo company.json que já está criado.
-
-  Essas informações devem ser adicionadas no HTML via Ajax.
-
-  Parte técnica:
-  Separe o nosso módulo de DOM criado nas últimas aulas em
-  um arquivo DOM.js.
-
-  E aqui nesse arquivo, faça a lógica para cadastrar os carros, em um módulo
-  que será nomeado de "app".
-  */
   function app(){
     var $companyElement = new DOM('[data-js="company"]');
     var $phoneElement = new DOM('[data-js="phone"]');
@@ -73,6 +40,8 @@
     }
 
     function loadAjaxApi(verb, url, params = null){
+      console.log(params);
+      console.log(url);
       ajaxLoadData = new XMLHttpRequest();
       ajaxLoadData.open(verb, url, true);
       if(verb === "POST"){
@@ -85,6 +54,7 @@
     }
 
     function loadData(){
+      clearTable();
       var ajax = loadAjaxApi('GET', 'http://localhost:3000/car');
 
       ajax.addEventListener('readystatechange', function(){
@@ -94,12 +64,21 @@
             populateTableCar(cars);
         }
       });
+
     }
 
     function populateTableCar(cars){
+      removeEventClick();
       cars.forEach(function(car){
         addRow(car)
-      })
+      });
+      addEventClickRemove();
+    }
+
+    function clearTable(){
+      Array.prototype.map.call($tableTBody.get()[0].children, function(item){
+        $tableTBody.get()[0].removeChild(item);
+      });
     }
 
     function isRequestOk(ajax){
@@ -107,7 +86,6 @@
     }
 
     function addRow(car){
-      console.log(car);
       cloneRow = $row.get()[0].cloneNode(true);
       $tableTBody.get()[0].appendChild(cloneRow);
 
@@ -116,8 +94,13 @@
       cloneRow.children[2].appendChild(doc.createTextNode(car.year));
       cloneRow.children[3].appendChild(doc.createTextNode(car.plate));
       cloneRow.children[4].appendChild(doc.createTextNode(car.color));
+      var newButton = doc.createElement("button");
+      newButton.setAttribute('class', 'remove');
+      newButton.setAttribute('data-id', car.plate);
+      newButton.appendChild(doc.createTextNode("Excluir"));
+      cloneRow.children[5].appendChild(newButton);
+      // cloneRow.children[5].childNodes[0].setAttribute('data-id', car.plate);
       clearForm();
-      addEventClickRemove();
     }
 
     function saveCar(){
@@ -126,6 +109,16 @@
       ajax.addEventListener('readystatechange', function(){
         if(isRequestOk(ajaxLoadData)){
           alert("Carro cadastrado com sucesso.");
+        }
+      });
+      loadData();
+    }
+
+    function deleteCar(carId){
+      var ajax = loadAjaxApi('DELETE', 'http://localhost:3000/car', carId);
+      ajax.addEventListener('readystatechange', function(){
+        if(isRequestOk(ajaxLoadData)){
+          alert("Carro excluído com sucesso.");
         }
       });
       loadData();
@@ -162,10 +155,12 @@
 
     function addEventClickRemove(){
       var $buttonsDelete = doc.querySelectorAll('.remove');
-      removeEventClick();
-      Array.prototype.forEach.call($buttonsDelete, function(button){
+      Array.prototype.map.call($buttonsDelete, function(button){
         button.addEventListener('click', function(){
-          $tableTBody.get()[0].removeChild(button.parentNode.parentNode);
+          var carId = button.getAttribute('data-id');
+          deleteCar("plate="+carId);
+          loadData();
+          // $tableTBody.get()[0].removeChild(button.parentNode.parentNode);
         }, false);
       });
     }
